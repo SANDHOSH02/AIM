6ac65079ee89e335cbc7a1ee24628cd83aa99fc1
Sandhosh <sandhoshtechie02@gmail.com>
2024-12-31 23:57:17 +0530

    header add sun

--- game-store/src/Componenets/Header.jsx
+++ game-store/src/Componenets/Header.jsx
@@ -3,18 +3,22 @@ import logo from './../assets/Images/logo.svg'
 
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 import { HiOutlineMoon } from "react-icons/hi2";
+import { HiOutlineSun } from "react-icons/hi2";
+
 
 
 
 function Header() {
   return (
-    <div className='flex items-center' >
+    <div className='flex items-center p-3 ' >
       <img src={logo} width={60} height={60} />
       <div className='flex bg-stone-200 p-2 w-full items-center mx-5 rounded-full  ' >
       <HiMiniMagnifyingGlass />
-        <input type='text' className='bg-transparent outline-none px-2 ' />
+        <input type='text' placeholder='Search Games' className='bg-transparent outline-none px-2 ' />
         <div>
         <HiOutlineMoon />
+        <HiOutlineSun />
+
         </div>    
 
         
